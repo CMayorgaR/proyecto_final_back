807dbfcf954df93870bc8f264e3ac33e142bec13
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-07-02 14:05:23 -0400

    repositorio base listo

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+app = Flask (__name__)
+db.init_app(app)
+CORS(app)
+#Migrate (app, db)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(BASEDIR, "test.db") #Dirección provisoria de la base de datos
+app.config['DEBUG'] = True
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000)
