6aba1753ab062fd605ba190c668d5626eb201cd1
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-07-05 20:21:51 -0400

    tablas de comidas listas

--- models.py
+++ models.py
@@ -1,3 +1,67 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+#TABLAS DE MENÚ
+class Starter(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    starter_name = db.Column (db.String(100), nullable= False)
+    starter_des = db.Column (db.String(300), nullable= False)
+
+    def __repr__(self):
+        return "<Starter %r>" % self.starter_name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.starter_name,
+            "description": self.starter_des
+            }
+
+class Main_Dish(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    main_name = db.Column (db.String(100), nullable= False)
+    main_des = db.Column (db.String(300), nullable= False)
+
+    def __repr__(self):
+        return "<Main_Dish %r>" % self.main_name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.main_name,
+            "description": self.main_des
+        }
+
+class Salad(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    salad_name = db.Column (db.String(100), nullable= False)
+    salad_des = db.Column (db.String(300), nullable= False)
+
+    def __repr__(self):
+        return "<Salad %r>" % self.salad_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.salad_name,
+            "description": self.salad_des
+        }
+
+class Dessert(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    dessert_name = db.Column (db.String(100), nullable= False)
+    dessert_des = db.Column (db.String(300), nullable= False)
+
+    def __repr__(self):
+        return "<Dessert %r>" % self.dessert_name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.salad_name,
+            "description": self.salad_des
+        }
+    
+#class Menu_Option(db.Model):
+    #id = db.Column (db.Integer, primary_key=True)
